339a1cadb388de24984bf5031f3d55d77a66d244
ultrons <sivaibhav@google.com>
2019-06-01 05:13:37 +0000

    updated data download script

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,10 @@
-cd ~/fairseq
-pip install --editable .
+## Install fairseq utils (Not needed if regular training script is not to be run)
+#cd `dirname $0`
+#pip install --editable .
+
+# Download dataset (Downloading smaller dataset only)
 mkdir ~/data/
 cd ~/data
 wget https://dl.fbaipublicfiles.com/fairseq/data/wmt18_en_de_bpej32k.zip && unzip wmt18_en_de_bpej32k.zip
-wget https://dl.fbaipublicfiles.com/fairseq/data/wmt18_en_de_bpej32k_btdata.zip && unzip wmt18_en_de_bpej32k_btdata.zip
-mv wmt18_en_de_bpej32k/* wmt18_en_de_bpej32k_btdata/
+#wget https://dl.fbaipublicfiles.com/fairseq/data/wmt18_en_de_bpej32k_btdata.zip && unzip wmt18_en_de_bpej32k_btdata.zip
+#mv wmt18_en_de_bpej32k/* wmt18_en_de_bpej32k_btdata/
